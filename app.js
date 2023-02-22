d6b970fc920b21cb12f5f51ee366a2679a14797a
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-02-22 20:30:47 +0530

    level 2 Encryption database

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.SECRET);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -15,19 +19,16 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
          
-
-
-
-
-
 app.get("/", function(req, res) {
     res.render("home");
 });
