5c0263c4ce8b296ef8661cbe5263cc936fce6ba9
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2023-02-23 16:38:07 +0530

    level-3 hashMethod using md5

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -24,7 +25,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,9 +43,10 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
+
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if (err) {
@@ -54,10 +56,10 @@ app.post("/register", function(req, res) {
         }
     }   );
 });
-
+console.log(md5("NIKHIL@123"));
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username},function(err, foundUser){
         if(err){
